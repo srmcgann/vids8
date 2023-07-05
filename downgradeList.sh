3583d5e2a306613e9f8b09dd66a5992d1288031a
scott mcgann <s.r.mcgann@hotmail.com>
2023-07-05 22:34:54 +0000

    initial commit

---
+++ downgradeList.sh
@@ -0,0 +1,6 @@
+#~/bin/bash
+./downgrade 'heterochromia [2PMQDpy9l14].mp4'  'heterochromia [2PMQDpy9l14]_downgraded.mp4'
+./downgrade 'Ariana Grande and Nicki Minaj - Side to Side (unicorn mix) [hUfhazqgsRI].mp4' 'Ariana Grande and Nicki Minaj - Side to Side (unicorn mix) [hUfhazqgsRI]_downgraded.mp4'
+./downgrade 'revival [z41bOSvepLo].mp4' 'revival [z41bOSvepLo]_downgraded.mp4'
+./downgrade 'antispace [jgqwNlJCo4Q].mp4' 'antispace [jgqwNlJCo4Q]_downgraded.mp4'
+./downgrade 'heavenly conduit [ifDQ7t-HJ_8].mp4' 'heavenly conduit [ifDQ7t-HJ_8]_downgraded.mp4'
